69cbc5f77162f3efd9e77c5969a0cc0b05b60bd9
XHao1997 <xuhao6815@gmail.com>
2024-09-15 14:11:38 +0800

    update qt style

--- my_app.py
+++ my_app.py
@@ -2,6 +2,16 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 from main_window_ui import Ui_MainWindow
+import signal
+
+def load_custom_style():
+    with open("style.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
+
+def exit_with_sigint():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,7 +21,8 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
+    load_custom_style()
+    exit_with_sigint()
     window = MainWindow()
     window.show()
 
