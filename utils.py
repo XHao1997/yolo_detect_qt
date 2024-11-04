369b666d81bbf4c4dbca08e65672d1c65744d8de
Hao Xu <xuhao6815@gmail.com>
2024-11-04 21:54:21 +0800

    change the method for predicting yolo_image

--- utils.py
+++ utils.py
@@ -40,6 +40,18 @@ def remap_dictionary(old_dict):
 
     print("Remapped dictionary:", remapped_number_dict)
     return remapped_number_dict
+
+def count_yolo_pred(result):
+    cls = result[0].boxes.cls
+    cls_name = np.unique(cls)
+    cls_num = np.unique(cls, return_counts=True)[1]
+    count_result = {}
+    for i,j in zip(cls_name,cls_num):
+        count_result[i] = j
+    return count_result
+
+
+
 if __name__ == "__main__":
     # Example input dictionary
     remap_dictionary({1: 1, 3: 11})  # 11 won't be found in the mapping
